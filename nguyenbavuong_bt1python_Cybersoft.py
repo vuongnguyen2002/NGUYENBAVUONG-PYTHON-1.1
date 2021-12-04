def4f771aee858c979d3825fa02401608e552966
vuongnguyen2002 <vuongnbgcs190189@fpt.edu.vn>
2021-12-04 22:06:53 +0700

    first commit

---
+++ nguyenbavuong_bt1python_Cybersoft.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+gia_theo_ngay = [10.1, 9.5, 10.7, 10.8, 10.6, 10.0, 10.2, 9.5]
+ds_gia_theo_ngay =[9.5, 10.7, 10.8, 10.6, 10.0, 10.2, 9.5]
+tong_gia_7 =  [ds*1.07 for ds in ds_gia_theo_ngay ]
+tong_gia_6 =[ds*1.06 for ds in ds_gia_theo_ngay ]
+   if tong_gia_7>10.1:
+    print ('OKEEE')
+        
+            
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
